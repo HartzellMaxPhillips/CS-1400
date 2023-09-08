ceab6f4edf49b85e14ff0dc2348a9bcd28817797
Max Phillips <11034905@uvu.edu>
2023-09-08 13:44:16 -0600

    Added Peter Quills first cut and corrected Rounding

--- Yondu.py
+++ Yondu.py
@@ -18,18 +18,27 @@ while True: #Made it opperate like money so you can have it rounded to 2 decimal
 piratesX = pirates - 2 
 #piratesX is pirates excluding Quill and Yondu
 
-#This is Yondu's 13% share
-units = units - piratesX * 3 
+#This is Yondu's 13% Total share
+units = units - piratesX * 3  #3 is the number of credits each pirate gets to take initially, Also PEMDAS makes multiplication go first
 yonduCut1 = units * 0.13 
 
-#this is the first place you can possible get a float value with more than two decimals
-round(units, 2) 
+#this is the first place you can possible get a float value with more than two decimals. It is necessary before adding to Yondu's cut.
+units = round(units, 2)
+yonduCut1 = round(yonduCut1, 2) #Using money logic
 units = units - yonduCut1
+
+#This is Peter Quill's 11% of the remaining share
+peterQuillCut1 = units * 0.11
+units = round(units, 2)
+peterQuillCut1 = round(peterQuillCut1, 2) #Using money logic
+units = units - peterQuillCut1
 
 
 
 
 yonduCutTotal = yonduCut1
+peterQuillTotal = peterQuillCut1
 
 print(f"current units: {units}")
 print(f"Yondu's share: {yonduCutTotal}")
+print(f"Peter's share: {peterQuillTotal}")
