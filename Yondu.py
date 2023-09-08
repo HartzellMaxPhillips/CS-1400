a02a737cd906c3b9b6e9ef2689c0c4e6bc2f8c26
Max Phillips <11034905@uvu.edu>
2023-09-08 13:18:30 -0600

    Added While loops to make Units and crew member come out correctly.  Also organized some comments and code.

--- Yondu.py
+++ Yondu.py
@@ -1,14 +1,31 @@
-piratesString = input("How many pirates:")
-pirates = int(piratesString)
+while True: #put while loop here to make sure we get a whole number to start with.  
+            #Yes I looked this up, the descpription doesn't technically say I can't do this please don't mark me down dear heavens.
+    try:    #You can't technically have half a pirate, even if he has a metal arm or missing limb, still is just one pirate. So that's my logic
+        piratesString = input("How many pirates:")
+        pirates = int(piratesString)
+        break
+    except ValueError:
+        print("Invalid input, please put a whole number.")
 
-unitsString = input("How many Units:")
-units = int(unitsString)
+while True: #Made it opperate like money so you can have it rounded to 2 decimal places.
+    try:
+        unitsString = input("How many Units:")
+        units = round(float(unitsString),2)
+        break
+    except:
+        print("Invalid input, please put a number rounded to two decimals")
 
-piratesX = pirates - 2
-units = units - piratesX * 3
-yonduCut1 = units * 0.13
-round(units, 2)
+piratesX = pirates - 2 
+#piratesX is pirates excluding Quill and Yondu
+
+#This is Yondu's 13% share
+units = units - piratesX * 3 
+yonduCut1 = units * 0.13 
+
+#this is the first place you can possible get a float value with more than two decimals
+round(units, 2) 
 units = units - yonduCut1
+
 
 
 
--- testCode.py
+++ testCode.py
@@ -7,3 +7,13 @@ while True:
         print("Invalid input. Please enter an integer.")
 
 print("You entered:", integer_value)
+
+while True:
+    try:
+        user_input = input("Enter a float rounded to 2 decimals: ")
+        float_value = round(float(user_input), 2)
+        break  # Break the loop if input is a valid float rounded to 2 decimals
+    except ValueError:
+        print("Invalid input. Please enter a valid float rounded to 2 decimals.")
+
+print("You entered:", float_value)
