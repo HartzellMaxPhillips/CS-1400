22686ca578daf62357ebcc5a3f5290e34d57d5db
Max Phillips <11034905@uvu.edu>
2023-09-22 11:49:41 -0600

    Working on turtle

--- TimmyTurtle.py
+++ TimmyTurtle.py
@@ -1,13 +1,13 @@
 import turtle #running everything in Turtle
 
-t = turtle.Turtle #setting t as a variable for our turtle
+t = turtle.Turtle() #setting t as a variable for our turtle
 
 def buildtriangle():
     for _ in range(3):
         t.forward(100)
         t.rt(120)
 
-buildtriangle
+buildtriangle()
 
 
 turtle.mainloop()
