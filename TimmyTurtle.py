0eef6056ef3c7f80404fdf20253af9ac2f360864
Max Phillips <11034905@uvu.edu>
2023-09-22 12:29:20 -0600

    saving TimmyTurtle

--- TimmyTurtle.py
+++ TimmyTurtle.py
@@ -1,13 +1,40 @@
-import turtle #running everything in Turtle
+from turtle import *#running everything in Turtle
+from random import *#getting some random number generator
 
-t = turtle.Turtle() #setting t as a variable for our turtle
+ #setting t as a variable for our turtle
+shape('turtle')
+pensize(2)
+speed(10)
 
-def buildtriangle():
-    for _ in range(3):
-        t.forward(100)
-        t.rt(120)
+def buildredtriangle():
+    pencolor("red")
+    fillcolor("red")
+    begin_fill()
+    for _ in range(2):
+        forward(100)
+        rt(120)
+    end_fill()
 
-buildtriangle()
+def buildcircle():
+    for _ in range(360):
+        forward(1)
+        rt(1)
+
+def buildsquare():
+    for _ in range(4):
+        forward(100)
+        rt(90)
+
+def randommove():
+    rand_forward = randrange(25, 300)
+    rand_left_turn = randrange(45, 180)
+    forward(rand_forward)
+    lt(rand_left_turn)
+
+buildredtriangle()
+randommove()
+buildsquare()
 
 
-turtle.mainloop()
+
+mainloop()
